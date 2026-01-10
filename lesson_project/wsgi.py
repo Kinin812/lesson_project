4af0a6654898da8dcd9f28341f4b26f298b69def
kirill <kinin@mail.ru>
2026-01-11 00:26:28 +0300

    update all.

---
+++ lesson_project/wsgi.py
@@ -0,0 +1,10 @@
+# lesson_project/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "lesson_project.settings",
+)
+
+application = get_wsgi_application()
