9b66add75fe6e3cb16b79c3512a56086d851cf2f
kirill <kinin@mail.ru>
2026-01-10 23:40:32 +0300

    add tasks.

---
+++ lessons/tasks.py
@@ -0,0 +1,13 @@
+# lessons/tasks.py
+import logging
+from celery import shared_task
+
+logger = logging.getLogger(__name__)
+
+@shared_task
+def send_lesson_notification(student_id: int, lesson_title: str):
+    logger.info(
+        "Уведомление отправлено студенту %s по уроку '%s'",
+        student_id,
+        lesson_title,
+    )
