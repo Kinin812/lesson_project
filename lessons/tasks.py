4938c8aa79df326a6ddabe723a2df744ff1c0b63
kirill <kinin@mail.ru>
2026-01-11 22:41:17 +0300

    reformat.

--- lessons/tasks.py
+++ lessons/tasks.py
@@ -6,8 +6,4 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def send_lesson_notification(student_id: int, lesson_title: str):
-    logger.info(
-        "Уведомление отправлено студенту %s по уроку '%s'",
-        student_id,
-        lesson_title,
-    )
+    logger.info(f"Уведомление отправлено студенту {student_id} по уроку {lesson_title}")
